329dcae56259914fd72b16052253ceaa90c449f6
Gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-06-20 17:58:07 -0300

    changed function click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
-const button = document.getElementsByClassName("mobile-responsive");
-const menu = document.getElementsByClassName("menu");
+// const button = document.getElementsByClassName("mobile-responsive");
+// const menu = document.getElementsByClassName("menu");
 
-button[0].addEventListener("click", () => {
-  //   button.classList.toggle("active");
-  menu.classList.toggle("active");
-});
+// button[0].addEventListener("click", () => {
+//     button.classList.toggle("active");
+//   menu.classList.toggle("active");
+// });
+
+const handleClick = () => {
+  const menu = document.querySelectorAll(".menu");
+  menu.classList.toggle("activate");
+};
